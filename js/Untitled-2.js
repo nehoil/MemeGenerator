0d98ad90672802625a3c6e6d5686bec26f0a6f04
Nehoray Ilani <neho12@gmail.com>
2020-10-24 20:57:22 +0300

    fix logo issue

--- js/Untitled-2.js
+++
@@ -1,7 +0,0 @@
-// Todos:
-// 1. Fix on change align, rec is demaged.
-// 2. Fix removal func so will not remove lines if they're not selected.
-// 3. Change drawLine to recieve lines.
-// 4. Chnage line 77 on meme-service to work with construction or find a work-around. (preffered)
-// 6. Consider change the canvas resize when live resizing is accure.
-// 7. Improve text onclick detection line 117 (find text hight with better technics) and improve detect if text is bigger
